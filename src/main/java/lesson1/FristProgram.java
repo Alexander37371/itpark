74fc59101e707ef26a114730d617f07406c839ab
Alexander37371 <nekit341310@mail.ru>
2021-11-05 18:00:29 +0300

    lesson 2

--- src/main/java/lesson1/FristProgram.java
+++ src/main/java/lesson1/FristProgram.java
@@ -1,6 +1,8 @@
+package lesson1;
+
 public class FristProgram {
 
     public static void main(String[] args) {
-        System.out.println("Привет, мир");
+        System.out.println("Привет мир");
     }
 }
