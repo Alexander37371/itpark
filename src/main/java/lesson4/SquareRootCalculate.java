1059c3fc65526f3dbc0a2b428db2109da24bd233
Alexander37371 <nekit341310@mail.ru>
2021-11-27 18:22:38 +0300

    4 lesson Home

--- src/main/java/lesson4/SquareRootCalculate.java
+++ src/main/java/lesson4/SquareRootCalculate.java
@@ -3,5 +3,18 @@ package lesson4;
 public class SquareRootCalculate {
     public static void main(String[] args) {
 
+        int square = 80; // Ввести число для вычисления квадратного корня
+        int possibleResult = -1;
+        for (int a = 0; a < square; a++) {
+            if (a * a == square) {
+                possibleResult = a;
+            }
+        }
+        if (possibleResult != -1) {
+            System.out.println("result = " + possibleResult);
+        } else {
+            System.out.println("result not found");
+        }
     }
 }
+
